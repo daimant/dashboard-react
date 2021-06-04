6504f57aece363fc87ca6fb75fac4a12b1bf3123
Sergey Pomortsev <zaooooza@gmail.com>
2021-06-04 15:21:11 +0300

    changed graphs type

--- src/Components/Widgets/Graph/Graph.tsx
+++ src/Components/Widgets/Graph/Graph.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import classes from "./Graph.module.scss";
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
+import {LineChart, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend, Line} from 'recharts';
 import {GraphProps} from "../../Common/Types";
 import WidgetsTitle from "../WidgetsTitle/WidgetsTitle";
 
 const Graph: React.FC<GraphProps> = ({sc, heightDisplay}) => {
   const {title, data, max, min} = sc;
-
+console.log(data)
   return (
     <div className={classes.graphs}>
       <p className={classes.title}>{title}</p>
       <ResponsiveContainer>
-        <AreaChart
+        <LineChart
           data={data}
           margin={{
             top: 0,
@@ -23,9 +23,12 @@ const Graph: React.FC<GraphProps> = ({sc, heightDisplay}) => {
           <CartesianGrid strokeDasharray="3 3"/>
           <XAxis dataKey="d"/>
           <YAxis domain={[min - 1, max + 1]}/>
-          <Area dataKey="v" stroke="#8884d8" fill="#8884d8"/>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="v1" stroke="#8884d8" />
+          <Line type="monotone" dataKey="v2" stroke="#82ca9d" />
           <Tooltip/>
-        </AreaChart>
+        </LineChart>
       </ResponsiveContainer>
     </div>
   )
