44d5c03979554f10465284bf2adee6b7edc644e9
Sergey Pomortsev <zaooooza@gmail.com>
2021-06-25 19:09:16 +0700

    added widgets types

--- src/Components/Widgets/Widgets.tsx
+++ src/Components/Widgets/Widgets.tsx
@@ -4,31 +4,32 @@ import KPKTable from "./KPKTable/KPKTable";
 import Graph from "./Graph/Graph";
 import {Preloader} from "../Common/Preloader/Preloader";
 import CircularBar from "./CircularBar/CircularBar";
+import {GraphType, KPKType, TodaysType} from "../Redux/widgets-reducer";
 
-export type GraphsType = {
-  id?: number
-  title: string
-  max: number
-  min: number
-  data: GraphElementsType[]
+type PropsType = {
+  kpk: KPKType
+  sc : GraphType[]
+  isFetchingWidgets: boolean
+  orgOid: string
+  period: string
+  periodType: string
+  kpkChild: KPKType
+  todays: TodaysType[]
+  tops: GraphType[]
+  requestKPKChild: (orgOid: string, period: string, periodType: string, serviceOid: string) => void
+  removeKPKChild: () => void
 }
-export type GraphElementsType = {
-  d: string
-  v: number
-}
 
-const Widgets: React.FC<any> = props => {
-  const {
-    kpk, sc, isFetchingWidgets, requestKPKChild, removeKPKChild, orgOid, period, periodType,
-    kpkChild, todays, tops
-  } = props;
-
+const Widgets: FC<PropsType> = ({
+                                  kpk, sc, isFetchingWidgets, requestKPKChild, removeKPKChild, orgOid, period,
+                                  periodType, kpkChild, todays, tops
+                                }) => {
   if (isFetchingWidgets) return <Preloader/>;
 
   return (
     <div>
       <main>
-        <KPKTable kpk={kpkChild.data ? kpkChild : kpk} requestKPKChild={requestKPKChild} removeKPKChild={removeKPKChild}
+        <KPKTable kpk={kpkChild.rows ? kpkChild : kpk} requestKPKChild={requestKPKChild} removeKPKChild={removeKPKChild}
                   orgOid={orgOid} period={period} periodType={periodType}/>
         <div className={classes.graphs}>
           <h4 className={classes.headCircularBar}>СЕГОДНЯ</h4>
@@ -37,13 +38,13 @@ const Widgets: React.FC<any> = props => {
           )}
         </div>
         <div className={classes.graphs}>
-          {sc.map((graph: GraphsType, i: number) =>
+          {sc.map((graph: GraphType, i: number) =>
             <Graph sc={graph} key={i}/>
           )}
         </div>
       </main>
       <div className={`${classes.secondMain}`}>
-        {tops.map((graph: GraphsType, i: number) =>
+        {tops.map((graph: GraphType, i: number) =>
           <Graph sc={graph} key={i} extendedStyle={{height: '100%'}}/>
         )}
       </div>
