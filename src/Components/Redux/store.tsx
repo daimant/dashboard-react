a19159d825214032dc948e152feb9df7b191aa4b
Sergey Pomortsev <zaooooza@gmail.com>
2021-06-25 16:44:22 +0700

    added redux-devtools

--- src/Components/Redux/store.tsx
+++ src/Components/Redux/store.tsx
@@ -1,14 +1,15 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import widgetsReducer from "./widgets-reducer";
 import filtersReducer from "./filters-reducer";
 import thunkMiddleware from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-let reducers = combineReducers({
+const reducers = combineReducers({
   filters: filtersReducer,
   widgets: widgetsReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 
 // export type RootState = ReturnType<typeof store.getState>;
 // export type DispatchType = typeof store.dispatch;
