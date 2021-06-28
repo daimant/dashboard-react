d12686d2b34f52ba817237f03f751f543cad73e0
Sergey Pomortsev <zaooooza@gmail.com>
2021-06-29 00:45:50 +0700

    refactor preloader

--- src/Components/Common/Preloader/Preloader.tsx
+++ src/Components/Common/Preloader/Preloader.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import classes from './Preloader.module.scss'
 
-export const Preloader = () => <div className={classes.preloader}>Идет загрузка...</div>;
+export const Preloader = ({title = 'Идет загрузка...'}) => <div className={classes.preloader}>{title}</div>;
