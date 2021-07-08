e06850c4a0a8cf41d482e917e76df88af83b051d
Sergey Pomortsev <zaooooza@gmail.com>
2021-07-08 23:34:21 +0700

    fixed graphArea value after dot err

--- src/Components/Widgets/GraphArea/GraphArea.tsx
+++ src/Components/Widgets/GraphArea/GraphArea.tsx
@@ -19,7 +19,7 @@ const GraphArea: FC<PropsType> = ({graphAreaData, extendedStyle = {}}) => {
           <YAxis tickFormatter={tick => `${tick * 100}`} tickCount={2}/>
           <Tooltip labelFormatter={(label: string) => `Дата: ${label}`}
                    // @ts-ignore
-                   formatter={(value: Array<number>, name: string) => ([`${percents[name]}: ${value[2] * 100} %`])}/>
+                   formatter={(value: Array<number>, name: string) => ([`${percents[name]}: ${Math.trunc(value[2] * 100)} %`])}/>
           <Area dataKey="p1" stroke="#8884d8" fill="#8884d8"/>
           <Area dataKey="p3" stroke="#FF0000" fill="#FF0000"/>
           <Area dataKey="p2" stroke="#82ca9d" fill="#82ca9d"/>
