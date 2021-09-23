7d091e49eb93566c914f3ea3fefa46064056fd5a
Sergey Pomortsev <zaooooza@gmail.com>
2021-09-23 20:04:20 +0500

    changed width tooltips

--- src/Components/Widgets/KPKTable/KPKTable.tsx
+++ src/Components/Widgets/KPKTable/KPKTable.tsx
@@ -69,7 +69,7 @@ export const LightTooltip = withStyles((theme: Theme) => ({
     backgroundColor: theme.palette.common.white,
     boxShadow: '0px 5px 5px -3px rgb(0 0 0 / 20%), 0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%)',
     fontSize: 12,
-    maxWidth: 350,
+    maxWidth: 370,
     color: theme.palette.common.black,
   },
 }))(Tooltip);
